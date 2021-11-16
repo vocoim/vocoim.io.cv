8b13376220776a1bf299c9be46bf064f60395767
vocoim <v1ka.tereshchenko@gmail.com>
2021-11-16 07:54:31 +0200

    add animation and script

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready () {
+    window.addEventListener('scroll', function () {
+        const el = document.querySelector(".languages-list");
+        const elDistance = el.getBoundingClientRect().top;
+       if(window.innerHeight > elDistance) {
+           document.querySelectorAll('span').forEach(abc => abc.style.animationPlayState = 'running');
+       }
+    })
+    show();
+    imageAnimation();
+    imageModal();
+}
+
+function show() {
+    const elements = document.querySelectorAll('.items-list_container');
+    elements.forEach(element => element.addEventListener("click", event => {
+        if (element.classList.contains('open')) {
+            element.classList.remove('open')
+        } else {
+            element.classList.add('open');
+        }
+    }))
+}
+
+function imageAnimation () {
+    const image = document.getElementById('modalImage');
+    image.addEventListener('mouseenter', event => {
+        image.style.animationPlayState = 'running';
+    })
+
+    image.addEventListener('mouseout', event => {
+        image.style.animationPlayState = 'paused';
+    })
+}
+
+function imageModal () {
+    const imageMod = document.getElementById('modalImage');
+    const modal = document.querySelector('.modal-window');
+
+    imageMod.addEventListener('click', touch => {
+        modal.style.display = 'block';
+        })
+
+        modal.addEventListener('click', hideModal => {
+            modal.style.display = 'none';
+        })
+}
+
+
+
+
